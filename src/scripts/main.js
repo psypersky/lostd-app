27126d71885b583e4b532389096cd3b2818f967c
espringe <ericwspringer@gmail.com>
2014-01-13 12:26:02 -0600

    Create initial layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,24 +3,57 @@ require(['react', 'pouchdb-nightly'], function(React, PouchDB) {
 
 	var db = new PouchDB('lostd');
 
+	db.allDocs({include_docs: true}, function(err, response) {
+
+		console.log('Response: ', response);
 
 
-	var Hello = React.createClass({
-	  render: function() {
-	    return (
-	      React.DOM.div({
-	        className: 'hello',
-	        children: 'Hello world! I am React!'
-	      })
-	    );
-	  }
 	});
 
-	Hello.renderComponent(
-	  CommentBox({}),
+
+	var Frame = React.createClass({
+		displayName: 'Frame',
+
+		getInitialState: function() {
+			return { view: 'people' };
+		},
+
+
+		render: function() {
+
+			var self = this;
+
+			function mkProperty(t) {
+
+				console.log('view: ', self.state.view);
+
+				return self.state.view === t ? {
+					id: 'activeFrameHeader'
+				} : {
+					onClick: function() {
+						self.setState({ view: t });
+					}
+				};
+			}
+
+
+			return (
+				React.DOM.div({ id: 'frame' },
+					React.DOM.div({ id: 'header ' },
+						React.DOM.div(mkProperty('people'), 'People'),
+						React.DOM.div(mkProperty('my_debts'), 'My Debts'),
+						React.DOM.div(mkProperty('owes_me'), 'Owes me')
+					)
+				)
+			);
+
+		}
+	});
+
+	React.renderComponent(
+	  Frame(null),
 	  document.getElementById('content')
 	);
-
 
 
 });
