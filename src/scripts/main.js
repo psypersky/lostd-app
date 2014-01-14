ec5f5873fdc192aa078a3cdc41aadeae9ff14f9b
espringe <ericwspringer@gmail.com>
2014-01-14 12:40:33 -0600

    Support adding and viewing friends

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,19 +9,8 @@ require(['react', 'pouchdb-nightly', 'lodash'], function(React, PouchDB, _) {
 
 	var db = new PouchDB('lostd');
 
-	console.log('current time:', _.now())
-
-
-	db.allDocs({include_docs: true}, function(err, response) {
-
-		console.log('Response: ', response);
-
-
-	});
-
-
-	var PeopleView = React.createClass({
-		displayName: 'PeopleView',
+	var FriendView = React.createClass({
+		displayName: 'FriendView',
 
 		getInitialState: function() {
 			return { data: [] };
@@ -30,9 +19,14 @@ require(['react', 'pouchdb-nightly', 'lodash'], function(React, PouchDB, _) {
 		componentWillMount: function() {
 			var self = this;
 
-			db.allDocs({include_docs: true}, function(err, response) {
+			function map(doc) {
+				if (doc.type === 'friend')
+					emit(doc._id, doc);
+			}
+
+			db.query({ map: map }, function(err, response) {
 				self.setState({
-					data: response.rows.map(function(x) { return x.doc; })
+					data: response.rows.map(function(x) { return x.value;  })
 				});
 
 			});
@@ -40,7 +34,7 @@ require(['react', 'pouchdb-nightly', 'lodash'], function(React, PouchDB, _) {
 
 		render: function() {
 
-			var list = this.state.data.map(function (x) {  return React.DOM.li({ key: x._id }, 'Person ', JSON.stringify(x));  });
+			var list = this.state.data.map(function (x) {  return React.DOM.li({ key: x._id }, 'Friend ', JSON.stringify(x));  });
 
 			return (
 				React.DOM.ul(null,
@@ -74,27 +68,52 @@ require(['react', 'pouchdb-nightly', 'lodash'], function(React, PouchDB, _) {
 			return (
 				React.DOM.div({ id: 'frame' },
 					React.DOM.ul({ id: 'header ' },
-						React.DOM.li(mkProperty('people'), 'People'),
+						React.DOM.li(mkProperty('friends'), 'Friends'),
 						React.DOM.li(mkProperty('my_debts'), 'My Debts'),
 						React.DOM.li(mkProperty('owes_me'), 'Owes me')
 					),
-					this.props.view === 'people' ? PeopleView(null) : null
+					this.props.view === 'friends' ? FriendView(null) : null
 				)
 			);
 		}
 	});
 
-	var Adder = React.createClass({
-		displayName: 'Adder',
+	var FriendAdder = React.createClass({
+		displayName: 'FriendAdder',
+
+		handleAdd: function() {
+			var self = this;
+			var name = self.refs.friendName.getDOMNode().value.trim();
+			if (!name)
+				return false;
+
+
+
+			// Import to disable it, to stop double-submitting..
+			self.refs.submitButton.getDOMNode().disabled = true;
+
+
+			db.post({
+				type: 'friend',
+				name: name,
+				added: _.now()
+			}, function (err) {
+				if (err)
+					console.error('Unable to add friend: ', name, ' got error: ', err);
+				else
+					self.props.onDone(true);
+			});
+
+			return false;
+		},
 
 		render: function() {
 			return (
-				React.DOM.form(null,
-					React.DOM.select(null,
-						React.DOM.option(null, 'Person'),
-						React.DOM.option(null, 'My debt'),
-						React.DOM.option(null, 'Owes me')
-					)
+				React.DOM.form({ id: 'adder', onSubmit: this.handleAdd },
+					React.DOM.h2(null, 'Add friend'),
+					React.DOM.input({ type: 'text', placeholder: 'Name', ref: 'friendName' }),
+					React.DOM.br(null),
+					React.DOM.input({ type: 'submit', value: 'Add friend!', ref: 'submitButton' })
 				)
 			);
 		}
@@ -107,7 +126,7 @@ require(['react', 'pouchdb-nightly', 'lodash'], function(React, PouchDB, _) {
 		getInitialState: function() {
 			return {
 				isAdding: false,
-				frameView: 'people'
+				frameView: 'friends'
 			};
 		},
 
@@ -120,11 +139,39 @@ require(['react', 'pouchdb-nightly', 'lodash'], function(React, PouchDB, _) {
 		},
 		
 		render: function() {
+
+			var text;
+			if (this.state.isAdding)
+				text = 'Cancel';
+			else {
+				text = 'Add ';
+				switch (this.state.frameView) {
+					case 'friends':
+						text += 'Friend';
+						break;
+					case 'my_debts':
+						text += 'a debt';
+						break;
+					case 'owes_me':
+						text += 'owed to me';
+						break;
+					default:
+						assert(false);
+				}
+			}
+
+			var widget;
+			if (this.state.isAdding) {
+				widget = FriendAdder({ onDone: this.toggleState });
+			} else {
+				widget = Frame({ view: this.state.frameView, onStateChanged: this.setViewState });
+			}
+
 			return (
 				React.DOM.div(null,
 					React.DOM.h1(null, 'Lostd App'),
-					React.DOM.a({ onClick: this.toggleState }, this.state.isAdding ? 'Cancel' : 'Add'),
-					(this.state.isAdding ? Adder(null) : Frame({ view: this.state.frameView, onStateChanged: this.setViewState }))
+					React.DOM.input({ type: 'button', onClick: this.toggleState, value: text }),
+					widget
 				)
 			);
 		}
